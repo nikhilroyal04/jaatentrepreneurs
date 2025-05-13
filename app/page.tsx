a68abba3ffe2552303028e9a9146142fc83fe6b0
Nikhil Chaudhary <154960134+nikhilroyal04@users.noreply.github.com>
2025-05-13 19:59:29 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <h1 className="text-3xl font-bold">Welcome to Jaat Entrepreneurs</h1>
+    </div>
+  );
+}
